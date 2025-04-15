d9268f19ff5084d46e66cbd16c1ed7a30e99d933
MOHAMED EL OUAFIR <mohamedoubaid999@gmail.com>
2025-04-15 17:57:58 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -8,7 +8,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 const Schema = mongoose.Schema;
 
 const personSchema = new Schema({
-  name : {type : string , required : true},
+  name : {type : String , required : true},
   age : Number,
   favoriteFoods : [String]
 });
