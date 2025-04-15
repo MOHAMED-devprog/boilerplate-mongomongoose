660fab10e839c119ea800fe0c7b9ccabcc2c3310
MOHAMED EL OUAFIR <mohamedoubaid999@gmail.com>
2025-04-15 18:29:51 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -52,7 +52,10 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById({_id : personId}, function(err, data){
+    if (err) return console.error("error");
+    done(null, data);
+  });
 };
 
 const findEditThenSave = (personId, done) => {
