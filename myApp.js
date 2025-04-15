6a82b79dc5ec50a4405634b35b3436d5cd5f56f8
MOHAMED EL OUAFIR <mohamedoubaid999@gmail.com>
2025-04-15 18:21:53 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -38,7 +38,10 @@ var createManyPeople = function(arrayOfPeople, done) {
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find(personName, function(err, data){
+    if (err) return console.log("error");
+    done(null , data);
+  });
 };
 
 const findOneByFood = (food, done) => {
