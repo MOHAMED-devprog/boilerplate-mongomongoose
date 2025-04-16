968c61855124bafe2057bb45ac0c4d28869137ff
MOHAMED EL OUAFIR <mohamedoubaid999@gmail.com>
2025-04-16 20:21:30 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -75,8 +75,10 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate({name : personName},{age : ageToSet}, {new : true}, (err, updatedPerson) => {
+    if (err) return console.error("error");
+    done(null ,updatedPerson);
+  })
 };
 
 const removeById = (personId, done) => {
